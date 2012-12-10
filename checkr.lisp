d8a6fd8b01c5ab446da25e07704c38688f0957d2
Albert Lee <hanzhupeng@gmail.com>
2012-12-10 16:00:26 +0800

    board marks

--- checkr.lisp
+++ checkr.lisp
@@ -7,35 +7,37 @@
   stone)
 
 (defparameter *board*
-  '((-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1  -1 1)
-    (-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1  1 1)
-    (-1 -1 -1 -1 -1 -1 -1 -1 -1 -1  1  1 1)
-    (-1 -1 -1 -1 -1 -1 -1 -1 -1  1  1  1 1)
-    (-1 -1 -1 -1 1 1 1 1 1 1 1 1 1  1  1  1  1)
-    (-1 -1 -1 -1 1 1 1 1 1 1 1 1 1  1  1  1)
-    (-1 -1 -1 -1 1 1 1 1 1 1 1 1 1  1  1)
-    (-1 -1 -1 -1 1 1 1 1 1 1 1 1 1  1)
-    (-1 -1 -1 -1 1 1 1 1 1 1 1 1 1)
-    (-1 -1 -1 1 1 1 1 1 1 1 1 1 1)
-    (-1 -1  1 1 1 1 1 1 1 1 1 1 1) 
-    (-1  1  1 1 1 1 1 1 1 1 1 1 1)
-    ( 1 1  1  1 1 1 1 1 1 1 1 1 1 )
-    (-1 -1  -1 -1 1 1 1 1)
-    (-1 -1  -1 -1 1 1 1)
-    (-1 -1  -1 -1 1 1)
-    (-1 -1 -1 -1 1)
+  '((nil nil nil nil nil nil nil nil nil nil nil nil 0)
+    (nil nil nil nil nil nil nil nil nil nil nil  1 2)
+    (nil nil nil nil nil nil nil nil nil nil  3 4 5)
+    (nil nil nil nil nil nil nil nil nil  6  7 8 9)
+    (nil nil nil nil * * * * * * * * * * * * *)
+    (nil nil nil nil * * * * * * * * * * * *)
+    (nil nil nil nil * * * * * * * * * * *)
+    (nil nil nil nil * * * * * * * * * *)
+    (nil nil nil nil * * * * * * * * *)
+    (nil nil nil * * * * * * * * * *)
+    (nil nil  * * * * * * * * * * *) 
+    (nil  * * * * * * * * * * * *)
+    ( * * * * * * * * * * * * * )
+    (nil nil nil nil g h i j)
+    (nil nil nil nil d e f)
+    (nil nil nil nil b c)
+    (nil nil nil nil a)
     ))
 
 (defun print-board ()
   ""
   (loop for i from 0 to 16 do
        (progn
-         (loop for i1 from 0 to i do
+         (loop for i1 from 0 to  i do
               (format t " "))
-         (loop for j from 0 to 18 do
+         (loop for j from 0 to 16 do
               (let ((v (nth j (nth i *board*))))
-                (if (and v (> v 0))
-                    (format t "O ")
+                (if v
+                    (if (equal v '*)
+                        (format t ". ")
+                        (format t "~D " v))
                     (format t "  "))))
                   (format t "~%"))))
 
@@ -43,5 +45,4 @@
   (format t "Start checkr..~%")
   (print-board))
 
-(main)
 
